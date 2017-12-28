320a3491ac14032e6d94d162d5288392a96bb785
JMP27 <marcin.ptaszny@gmail.com>
2017-12-28 10:14:33 +0100

    New constructor

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -4,4 +4,12 @@ public class FirstClass {
     String a = null;
     String b = null;
     String c = null;
+
+    public FirstClass(){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
 }
