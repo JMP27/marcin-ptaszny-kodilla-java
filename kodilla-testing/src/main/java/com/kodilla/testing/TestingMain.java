e4b7ef40f6791db475bbe141c04affdea4247d61
JMP27 <marcin.ptaszny@gmail.com>
2018-01-10 19:51:16 +0100

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String args[]){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error");
+        }
+
+        //Second test
+
+        Calculator calculator = new Calculator();
+
+        int add = calculator.add(1,1);
+        int sub = calculator.sub(2,1);
+
+        System.out.println("Sum Test: " + add);
+        System.out.println("Subtract Test: " + sub);
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int a = 0;
+    private int b = 0;
+
+    public Calculator(){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b){
+     return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public int getA(){
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
