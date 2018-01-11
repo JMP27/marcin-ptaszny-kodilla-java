9d2d307e874c3e3a9df4182ecbcb3ac78d4ffc94
JMP27 <marcin.ptaszny@gmail.com>
2018-01-11 08:55:19 +0100

    No one

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,24 +6,5 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String args[]){
 
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if(result.equals("theForumUser")){
-            System.out.println("Test OK");
-        }else{
-            System.out.println("Error");
-        }
-
-        //Second test
-
-        Calculator calculator = new Calculator();
-
-        int add = calculator.add(1,1);
-        int sub = calculator.sub(2,1);
-
-        System.out.println("Sum Test: " + add);
-        System.out.println("Subtract Test: " + sub);
     }
 }
