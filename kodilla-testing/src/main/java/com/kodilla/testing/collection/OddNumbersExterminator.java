95fe2cc1dd776ec1d37431a5a57a395670e0daa9
JMP27 <marcin.ptaszny@gmail.com>
2018-01-12 09:23:52 +0100

    Task corrected in accordance with the guidelines

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -14,15 +14,11 @@ public class OddNumbersExterminator {
 
     public ArrayList<Integer> exterminate (ArrayList<Integer> numbers){
         ArrayList<Integer> evenNumbers = new ArrayList<>();
-        ArrayList<Integer> oddNumbers = new ArrayList<>();
+
         for(int i=0 ; i<numbers.size();i++){
             int a = numbers.get(i);
             if(a%2 == 0){
                 evenNumbers.add(numbers.get(i));
-
-            }else{
-                oddNumbers.add(numbers.get(i));
-
             }
         }
         return evenNumbers;
