732a8803723111babde7ec0e1d285303defc608d
JMP27 <marcin.ptaszny@gmail.com>
2018-02-16 10:14:18 +0100

    Module 10.3 - Portfolio

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,6 +18,9 @@ public class BoardTestSuite {
         board.doneList.tasks.add("Task 3");
 
         //Then
+        Assert.assertEquals("Task 1", board.toDoList.tasks.get(0));
+        Assert.assertEquals("Task 2", board.inProgressList.tasks.get(0));
+        Assert.assertEquals("Task 3", board.doneList.tasks.get(0));
         System.out.println(board);
 
     }
