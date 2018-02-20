0eb83f794d5bbc49f21005206f39b43d9ced83f9
JMP27 <marcin.ptaszny@gmail.com>
2018-02-20 21:11:38 +0100

    Module 11.1 - New one version, singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @Test
+    public void loggerTestSuite(){
+
+        Logger.getInstance().log("test1");
+        Logger.getInstance().log("test2");
+        Logger.getInstance().log("test3");
+
+        String logs = Logger.getInstance().getLastLog();
+        Assert.assertEquals("test3", logs);
+        System.out.println(logs);
+    }
+}
