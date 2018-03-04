d9cd659b7184b01c364ae2537ffae2b83f7ac3ae
JMP27 <marcin.ptaszny@gmail.com>
2018-03-04 20:59:39 +0100

    Module 13.1 - Maps object

---
+++ kodilla-sprint-web/src/main/java/com/kodilla/sprint/web/KodillaSprintWebApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.sprint.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSprintWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSprintWebApplication.class, args);
+	}
+}
